1e487d84638a1efb7c1519f2cb76301c8666c6fe
chasey-dev <ellenyoung0912@gmail.com>
2026-05-04 23:42:45 +0800

    mediatek: filogic: add support for SL-3000 eMMC
    Signed-off-by: chasey-dev <ellenyoung0912@gmail.com>

--- package/mtk/applications/mtk-smp/files/smp.sh
+++ package/mtk/applications/mtk-smp/files/smp.sh
@@ -811,6 +811,7 @@ setup_model()
 	openwrt,one |\
 	qihoo,360t7 |\
 	routerich,ax3000* |\
+	sl,3000-emmc |\
 	snr,snr-cpe-ax2 |\
 	teltonika,rutc50 |\
 	tenbay,wr3000k |\
--- target/linux/mediatek/image/filogic-ext.mk
+++ target/linux/mediatek/image/filogic-ext.mk
@@ -59,6 +59,19 @@ define Device/netcore_n60-pro-mtkuboot
 endef
 TARGET_DEVICES += netcore_n60-pro-mtkuboot
 
+define Device/sl_3000-emmc
+  DEVICE_VENDOR := SL
+  DEVICE_MODEL := 3000 eMMC
+  DEVICE_DTS := mt7981b-sl-3000-emmc
+  DEVICE_DTS_DIR := ../dts-ext
+  DEVICE_PACKAGES := e2fsprogs f2fsck mkf2fs
+  KERNEL := kernel-bin | lzma | fit lzma $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb
+  KERNEL_INITRAMFS := kernel-bin | lzma | \
+	fit lzma $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb with-initrd | pad-to 64k
+  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+endef
+TARGET_DEVICES += sl_3000-emmc
+
 define Device/xiaomi_mi-router-ax3000t-mtkuboot
   DEVICE_VENDOR := Xiaomi
   DEVICE_MODEL := Mi Router AX3000T
